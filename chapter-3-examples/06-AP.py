84b0dfd4246b7af54c0e4b65973b0700845a6d96
Kabir Srivastava <kabirsrivastava12@gmail.com>
2021-07-12 15:42:27 +0530

    []

--- chapter-3-examples/06-AP.py
+++ chapter-3-examples/06-AP.py
@@ -1,11 +1,9 @@
-
-
+# First 4 terms of an A.P: a, a+d, a+2*d, a+3*d
 
 def AP(intialValue,step):
     return intialValue, intialValue + step, intialValue + 2 * step,intialValue + 3 * step
 
   
-
 initialValue = int(input("Enter Initial Value: "))
 step = int(input("Enter Step: "))
 
