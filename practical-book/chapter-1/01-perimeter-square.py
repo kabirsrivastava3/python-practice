15b3c517a54968abd938b7c4aa8d6aa396c4822c
Kabir Srivastava <84907889+kabirsrivastava3@users.noreply.github.com>
2021-08-10 11:53:09 +0530

    Delete practical-book directory

--- practical-book/chapter-1/01-perimeter-square.py
+++
@@ -1,5 +0,0 @@
-#perimeter of a square
-
-perimeter = float(input("Enter the perimeter of a square: "))
-side = perimeter / 4
-print("The side of the square is",side)
--- practical-book/chapter-1/05-inverted-traingle.py
+++
@@ -1,7 +0,0 @@
-
-
-
-for row in range(1,11):
-  for column in range(11,row - 1,-1):
-    print('*', end = " ")
-  print(" \n ")
