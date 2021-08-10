9c2fc42ad1411f106996ce918b38bfd4685319e8
Kabir Srivastava <84907889+kabirsrivastava3@users.noreply.github.com>
2021-08-10 11:57:44 +0530

    Delete chapter-2 directory

--- chapter-2/01-telephone-number.py
+++
@@ -1,9 +0,0 @@
-# check for valid phone number and display it
-#Format: 017-555-1212
-
-phoneNumber = input("enter your phone number = ")
-length = len(phoneNumber)
-if length == 12 and phoneNumber[:3].isdigit() and phoneNumber[3]== "-" and phoneNumber[4:7].isdigit() and phoneNumber[7]== "-" and phoneNumber[8:13].isdigit():
-    print("It is vaild Phone-Number",phoneNumber)
-else :
-    print("It is not vaild Phone-Number")
--- chapter-2/08-tuple-fibo-9.py
+++
@@ -1,9 +0,0 @@
-#print the first 9 terms of Fibonacci series
-#Time Complexity = O(N)
-
-fiboSeries= (0,1,1)
-
-for i in range(6):
-    fiboSeries = fiboSeries + (fiboSeries[i+1] + fiboSeries[i+2],)
-
-print("9 terms of Fibonacci series are: ",fiboSeries)
