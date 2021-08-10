61ae84a6ff5b4441e97fb47df8efb6190eafaa80
Kabir Srivastava <84907889+kabirsrivastava3@users.noreply.github.com>
2021-08-10 11:55:27 +0530

    Delete chapter-1-examples directory

--- chapter-1-examples/02-square-root.py
+++
@@ -1,5 +0,0 @@
-#square root of number 
-
-number = float(input("Enter a number: "))
-sq_root = number ** 0.5
-print("Square Root of "+str(number)+" is "+str(sq_root))
--- chapter-1-examples/04-cube-15-to-20.py
+++
@@ -1,7 +0,0 @@
-#program for cube of numbers from 15-20
-
-#Time Complexity = O(N)
-
-for i in range(15,21):
-        print(str(i)+ " " + str(i*i*i))
-
